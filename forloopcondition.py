2f84c76b0c1194e83de98a5b8a5e5eb561ff439e
admin <cgoutham@gmail.com>
2025-02-03 18:10:56 +0530

    added

--- forloopcondition.py
+++ forloopcondition.py
@@ -1,5 +1,52 @@
-# example
-# s=input("Enter the name: ")
+# example 1
+# s=eval(input("Enter the name: "))
 # for a in s:
 #     print(a)
+
+
+# example 2
+# how to check the total number of charchters present  in the given string 
+
+# cha=eval(input("Enter the name:"))
+# count=0
+# for a in cha:
+#     count+=1 ##------------> count = count + 1
+#     print(a)
+# print("the number of char prenent in the name is ", count) 
+
+# example 3
+# to print the character present in the string in  idex wise 
+
+# name = input("Enter the name: ")
+# index=0
+# for x in name:
+#     print("The character present at",index,"index is ",x)
+#     index+=1
+
+
+
+# example 4
+# print hello 10 times 
+
+# s=int(input("enter the vale:"))
+# n="hello"
+# cou=0
+# for x in range(s):
+#     cou+=1
+#     print(cou,n)
+
+
+
+
+# assgn 1
+# By using the  for loop 
+# a) to display odd number for 0 to 20
+# b) print even number  B/W 40 and 100 
+# c) display number from 10 to 1 in descending order 
+
+
+for x in  range(20):
+    
+    a=int(x%2!=0)
+    print(a)
     
