8c9a5bdf0d0281b97682f9f621390cca17c77ebb
admin <cgoutham@gmail.com>
2025-02-11 16:33:34 +0530

    added

--- flask_curd_app/config.py
+++ flask_curd_app/config.py
@@ -1,4 +1,4 @@
 # # config.py
 # #DATABASE_URI = 'postgresql://your_username:your_password@localhost:7050/fts_new_v5'
-# #DATABASE_URI = 'postgresql://iotpf-v3-superuser:XHB6j3HPDW3Ftn@iot.hyperthings.in:2220/test_db'
+#DATABASE_URI = 'postgresql://iotpf-v3-superuser:XHB6j3HPDW3Ftn@iot.hyperthings.in:2220/test_db'
 # DEBUG = True
